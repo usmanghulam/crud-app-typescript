5db440adb2dd631a170922dd8b1f3658794effa6
Usman Ghulam <usmanghulam01@gmail.com>
2021-10-07 19:36:02 +0500

    implement folder structure

---
+++ src/crud-app/components/create-user-btn.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+
+const CreateUserButton = (props: object) => {
+    return <Button {...props} />;
+};
+
+export default CreateUserButton;
